32a5b6d5cc11a07d7e5a96217f44654aa83a3a20
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-20 06:21:04 +0200

    get OBR Token

--- app/Http/Controllers/SendInvoiceToOBR.php
+++ app/Http/Controllers/SendInvoiceToOBR.php
@@ -8,17 +8,29 @@ use Illuminate\Support\Facades\Http;
 class SendInvoiceToOBR extends Controller
 {
     //
+    private string $baseUrl = 'http://41.79.226.28:8345/ebms_api/';
 
     public function __construct(){
-        $req =  Http::acceptJson()->post('http://41.79.226.28:8345/ebms_api/login/', [
-                    'username' => 'ws400206064000267',
-                    'password' => '69l_Gy6H'
-                ]);
+       
+      echo($this->getToken());
+      dd("je suis");
+    }
 
+    public function getToken(string $username ='ws400206064000267',string $password ='69l_Gy6H') 
+    {
+        $req =  Http::acceptJson()->post($this->baseUrl.'login/', [
+                    'username' => $username,
+                    'password' => $password
+                ]);
         $response = json_decode($req->body());
 
-        dump($response->success);
+        $success = $response->success;
+        $message = $response->msg;
+        $token = "";
+        if($success ){
+          $token = $response->result->token; 
+        }
 
-        dd("je suis un Millionnaire");
+        return $token;
     }
 }
