35d7dd46ac1ab953e47acc82f2ffd3b3eb9ec536
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-20 06:33:08 +0200

    get Invoince

--- app/Http/Controllers/SendInvoiceToOBR.php
+++ app/Http/Controllers/SendInvoiceToOBR.php
@@ -11,26 +11,32 @@ class SendInvoiceToOBR extends Controller
     private string $baseUrl = 'http://41.79.226.28:8345/ebms_api/';
 
     public function __construct(){
-       
-      echo($this->getToken());
-      dd("je suis");
+
+        $token = $this->getToken();
+        
+        $req =  Http::withToken($token)->acceptJson()->post($this->baseUrl.'getInvoice/',[
+            'invoice_signature' => ''
+        ]);
+
+        dump($req->body());
+        dd("je suis");
     }
 
+    // Generation du TOken
     public function getToken(string $username ='ws400206064000267',string $password ='69l_Gy6H') 
     {
         $req =  Http::acceptJson()->post($this->baseUrl.'login/', [
-                    'username' => $username,
-                    'password' => $password
-                ]);
+            'username' => $username,
+            'password' => $password
+        ]);
         $response = json_decode($req->body());
-
         $success = $response->success;
         $message = $response->msg;
         $token = "";
         if($success ){
           $token = $response->result->token; 
-        }
+      }
 
-        return $token;
-    }
+      return $token;
+  }
 }
