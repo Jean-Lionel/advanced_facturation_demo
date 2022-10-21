17303a99a6462b45c85ccc906ef9fe87468435ba
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-21 05:16:25 +0200

    created file stock

--- resources/views/journals/fiche_stock.blade.php
+++ resources/views/journals/fiche_stock.blade.php
@@ -4,4 +4,24 @@
 
 @section('content')
 
+<div>
+	<table>
+		<thead>
+			<tr>     
+				<th>Code</th>
+				<th>Article</th>
+				<th>Unité</th>
+				<th>St.Initial</th>
+				<th>Entrées</th>
+				<th>Sorties</th>
+				<th>St.Théoriq.</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			
+		</tbody>
+	</table>
+</div>
+
 @stop
