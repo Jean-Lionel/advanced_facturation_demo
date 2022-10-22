5a61aab0ccd5ba8b3f341281c6e4c915cb6582f1
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-22 04:38:33 +0200

    adding file for created entreprise

--- app/Http/Livewire/Entreprise/EntrepriseComponent.php
+++ app/Http/Livewire/Entreprise/EntrepriseComponent.php
@@ -6,6 +6,26 @@ use Livewire\Component;
 
 class EntrepriseComponent extends Component
 {
+
+    public $tp_name = "";
+    public $tp_type = "";
+    public $tp_TIN = "";
+    public $tp_trade_number = "";
+    public $tp_postal_number = "";
+    public $tp_phone_number = "";
+    public $tp_address_privonce = "";
+    public $tp_address_quartier = "";
+    public $tp_address_avenue = "";
+    public $tp_address_rue = "";
+    public $tp_address_number = "";
+    public $vat_taxpayer = "";
+    public $ct_taxpayer = "";
+    public $tl_taxpayer = "";
+    public $tp_fiscal_center = "";
+    public $tp_activity_sector = "";
+    public $tp_legal_form = "";
+    public $payment_type = "";
+
     public function render()
     {
         return view('livewire.entreprise.entreprise-component');
--- resources/views/livewire/entreprise/entreprise-component.blade.php
+++ resources/views/livewire/entreprise/entreprise-component.blade.php
@@ -1,4 +1,27 @@
 <div>
     {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
     <h1>Entre prise X</h1>
+
+    <div class="row">
+        <form action="">
+            <input class="form-control  col-md-4 form-control-sm" wire:model="tp_name" placeholder="" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_type" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_TIN" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_trade_number" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_postal_number" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_phone_number" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_address_privonce" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_address_quartier" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_address_avenue" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_address_rue" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_address_number" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="vat_taxpayer" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="ct_taxpayer" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tl_taxpayer" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_fiscal_center" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_activity_sector" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="tp_legal_form" />
+         <input class="form-control  col-md-4 form-control-sm" wire:model="payment_type" />
+     </form>
+ </div>
 </div>
