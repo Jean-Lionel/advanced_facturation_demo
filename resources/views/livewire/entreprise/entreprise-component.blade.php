f4c5131f3ba4b92a3bc1e6dcc9685c7c847e6505
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-22 05:21:05 +0200

    field list

--- resources/views/livewire/entreprise/entreprise-component.blade.php
+++ resources/views/livewire/entreprise/entreprise-component.blade.php
@@ -54,25 +54,48 @@
                <label for="">Adresse du contribuable : Province</label>
                <input class="form-control  form-control-sm" wire:model="tp_address_privonce" />
            </div>
-            <div class="from-group col-md-4">
+           <div class="from-group col-md-4">
                <label for="">Adresse du contribuable : commune</label>
                <input class="form-control  form-control-sm" wire:model="tp_address_commune" />
            </div>
-        <div class="from-group col-md-4">
-               <label for=""></label>
+           <div class="from-group col-md-4">
+               <label for="">Adresse du contribuable : quartier </label>
+               <input class="form-control  form-control-sm" wire:model="tp_address_quartier" />
            </div>
+           <div class="from-group col-md-4">
+               <label for="">Adresse du contribuable : Avenue </label>
+               <input class="form-control  form-control-sm" wire:model="tp_address_avenue" />
+           </div>
+           <div class="from-group col-md-4">
+               <label for="">Adresse du contribuable : Rue </label>
+               <input class="form-control  form-control-sm" wire:model="tp_address_rue" />
+           </div>
+           <div class="from-group col-md-4">
+               <label for="">Adresse du contribuable : Numéro </label>
+               <input class="form-control  form-control-sm" wire:model="tp_address_number" />
+           </div>
+           <div class="from-group col-md-4 ">
+            <input type="checkbox" id="vat_taxpayer" class="form-control-sm" wire:model="vat_taxpayer" />
+               <label for="vat_taxpayer">Assujetti à la TVA</label>
+               
+           </div>
+        
+         <div class="from-group col-md-4">
+            <input type="checkbox" id="ct_taxpayer" class="form-control-sm" wire:model="ct_taxpayer" />
+             <label for="ct_taxpayer">Assujetti à la taxe de consommation</label>
+             
+         </div> 
 
-           <input class="form-control  form-control-sm" wire:model="tp_address_quartier" />
-           <input class="form-control  form-control-sm" wire:model="tp_address_avenue" />
-           <input class="form-control  form-control-sm" wire:model="tp_address_rue" />
-           <input class="form-control  form-control-sm" wire:model="tp_address_number" />
-           <input class="form-control  form-control-sm" wire:model="vat_taxpayer" />
-           <input class="form-control  form-control-sm" wire:model="ct_taxpayer" />
-           <input class="form-control  form-control-sm" wire:model="tl_taxpayer" />
-           <input class="form-control  form-control-sm" wire:model="tp_fiscal_center" />
-           <input class="form-control  form-control-sm" wire:model="tp_activity_sector" />
-           <input class="form-control  form-control-sm" wire:model="tp_legal_form" />
-           <input class="form-control  form-control-sm" wire:model="payment_type" />
-       </form>
-   </div>
+     </form>
+
+
+   
+   <input class="form-control  form-control-sm" wire:model="ct_taxpayer" />
+   <input class="form-control  form-control-sm" wire:model="tl_taxpayer" />
+   <input class="form-control  form-control-sm" wire:model="tp_fiscal_center" />
+   <input class="form-control  form-control-sm" wire:model="tp_activity_sector" />
+   <input class="form-control  form-control-sm" wire:model="tp_legal_form" />
+   <input class="form-control  form-control-sm" wire:model="payment_type" />
+</form>
+</div>
 </div>
