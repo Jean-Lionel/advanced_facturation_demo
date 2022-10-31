56061dbc3cd886226e5a095f64a274c853f59bb2
Jean-Lionel <nijeanlionel@gmail.com>
2022-10-31 05:56:28 +0200

    display info

--- resources/views/entreprises/index.blade.php
+++ resources/views/entreprises/index.blade.php
@@ -3,11 +3,7 @@
 @section('content')
 
 <div>
-	<div class="text-right">
-		<a href="" class="btn btn-link">Entreprise</a>
-		<a href="{{ route('obr_declarations.index') }} " class="btn btn-link">Déclaration Obr</a>
-		<a href="" class="btn btn-link">Historique de déclaration Obr</a>
-	</div>
+	@include('entreprises.header')
 	@livewire("entreprise.entreprise-component")
 </div>
 @stop
--- resources/views/obr_declarations/index.blade.php
+++ resources/views/obr_declarations/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
 <div>
+	@include('entreprises.header')
 	<h5>Facture en attente</h5>
 	<table class="table table-bordered tab-content table-sm">
 		<thead>
@@ -21,35 +22,40 @@
 		<tbody>
 			
 			@foreach ($orders as $order)
-				{{-- expr --}}
-				<tr>
-					<td>{{$order->id}}</td>
-					<td>{{ $order->client->name }}</td>
-					<td>{{ $order->amount }}</td>
-					<td>{{ $order->tax }}</td>
-					<td>
-						<ul>
-							<li  class="d-flex justify-content-between">
-								<b>Désignation</b>
-								<b>qté</b>
-								<b>Prix</b>
-							</li>
-							@foreach ($order->products as $element)
-								{{-- expr --}}
-							<li class="d-flex justify-content-between">
-								<span>{{ $element['name'] }}</span>
-								<span>{{ $element['quantite'] }}</span>
-								<span>{{ $element['price'] }}</span>
-							</li>
-								
-							@endforeach
-						</ul>
-					</td>
-					<td>{{ $order->created_at }}</td>
-					<td>
-						<button onclick="sendInvoice({{$order->id}})">Envoyer</button>
-					</td>
-				</tr>
+			{{-- expr --}}
+			<tr>
+				<td>{{$order->id}}</td>
+				<td>{{ $order->client->name }}</td>
+				<td>{{ $order->amount }}</td>
+				<td>{{ $order->tax }}</td>
+				<td>
+					<ul>
+						<li  class="d-flex justify-content-between">
+							<b>Désignation</b>
+							<b>qté</b>
+							<b>Prix</b>
+						</li>
+						@foreach ($order->products as $element)
+						{{-- expr --}}
+						<li class="d-flex justify-content-between">
+							<span>{{ $element['name'] }}</span>
+							<span>{{ $element['quantite'] }}</span>
+							<span>{{ $element['price'] }}</span>
+						</li>
+
+						@endforeach
+					</ul>
+				</td>
+				<td>{{ $order->created_at }}</td>
+				<td>
+					<div id="button_{{$order->id}}">
+						<button  onclick="sendInvoice({{$order->id}})">Envoyer</button>
+					</div>
+					
+
+					
+				</td>
+			</tr>
 			@endforeach
 		</tbody>
 	</table>
@@ -60,14 +66,22 @@
 
 <script>
 	function sendInvoice(invoince_id){
+		 $("#button_"+invoince_id).html(`
+		 	<div  class="spinner-border text-primary" role="status">
+						<span class="sr-only">Loading...</span>
+					</div>
+		 	`)
 		
 		$.ajax({
-				url: 'sendInvoinceToObr/'+invoince_id,
-				type: 'get',
-				success: function (data) {
-					console.log(data);
-				}
-			});
+			url: 'sendInvoinceToObr/'+invoince_id,
+			type: 'get',
+			success: function (data) {
+				console.log(data)
+				$("#button_"+invoince_id).html(`
+					<div class="${data.success ? 'bg-primary' :'bg-danger'}"> ${data.msg}</div>
+					`)
+			}
+		});
 	}
 </script>
 
