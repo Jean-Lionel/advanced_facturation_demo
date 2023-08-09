2f84e5bd4466a24149b396cd8a5c8d656a34715f
Jean-Lionel <nijeanlionel@gmail.com>
2023-08-09 12:10:07 +0200

    return product

--- app/Http/Livewire/ObrStock/RetourProduct.php
+++ app/Http/Livewire/ObrStock/RetourProduct.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class RetourProduct extends Component
 {
+    public $factureNumber;
+
+    public function searchFacture(){
+        dd($this->factureNumber);
+    }
     public function render()
     {
         return view('livewire.obr-stock.retour-product');
--- resources/views/livewire/obr-stock/retour-product.blade.php
+++ resources/views/livewire/obr-stock/retour-product.blade.php
@@ -1,4 +1,17 @@
 <div>
     {{-- The whole world belongs to you. --}}
-    <h1>Je suis en bonne du forme</h1>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <input type="text" placeholder="Numéro du facture" wire:model='factureNumber'
+                wire:keyup.enter='searchFacture'
+                />
+                <button wire:click='searchFacture'>Rechercher</button>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Title</h5>
+                <p class="card-text">Content</p>
+            </div>
+        </div>
+    </div>
 </div>
