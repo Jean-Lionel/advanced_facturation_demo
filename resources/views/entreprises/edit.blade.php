cf5ad3d941a052514ca8f7f6a52eb199119288a4
Jean-Lionel <nijeanlionel@gmailcom>
2024-02-29 05:52:58 +0200

    fix time out

--- resources/views/entreprises/edit.blade.php
+++ resources/views/entreprises/edit.blade.php
@@ -6,43 +6,49 @@
             @csrf
             @method('PUT')
             <h5 class="row p-2 text-center">Modification Entreprise</h5>
+            <div class="d-flex justify-content-between">
+                <h4>{{ $entreprise->tp_name }}</h4>
+                <div>
+                    <h6> NIF : {{ $entreprise->tp_TIN  }}</h6>
+                    <h6> R.C : {{ $entreprise->tp_trade_number  }}</h6>
+                </div>
+            </div>
             <div class="form-group row">
-                <div class="col-4">
-                    <label for="">TP_NAME</label>
-                    <input type="text" name="tp_name" id="tp_name" class="form-control"
+                <div class="col-0">
+                    <input type="text" hidden  name="tp_name" id="tp_name" class="form-control"
                         value="{{ $entreprise->tp_name }}">
                 </div>
-                <div class="col-4">
-                    <label for="">TP_TIN {{ $entreprise->tp_TIN  }}</label>
+                <div class="col-0">
+                    {{-- <label for="">TP_TIN {{ $entreprise->tp_TIN  }}</label> --}}
                     <input type="text" hidden name="tp_tin" id="tp_tin" class="form-control"
                         value="{{ $entreprise->tp_TIN }}">
                 </div>
                 <div class="col-4">
-                    <label for="">TP_POSTAL_NUMBER</label>
+                    <label for="">BOITE POSTAL</label>
                     <input type="text" name="tp_postal_number" id="tp_postal_number" class="form-control"
                         value="{{ $entreprise->tp_postal_number }}">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-4">
-                    <label for="">TP_ADDRESS_PRIVONCE</label>
+                    <label for="">PROVINCE</label>
                     <input type="text" name="tp_address_privonce" id="tp_address_privonce" class="form-control"
                         value="{{ $entreprise->tp_address_privonce }}">
                 </div>
                 <div class="col-4">
-                    <label for="">TP_ADDRESS_AVENUE</label>
+                    <label for="">AVENUE</label>
                     <input type="text" name="tp_address_avenue" id="tp_address_avenue" class="form-control"
                         value="{{ $entreprise->tp_address_avenue }}">
                 </div>
                 <div class="col-4">
-                    <label for="">TP_ADDRESS_NUMBER</label>
+                    <label for="">NUMERO</label>
                     <input type="text" name="tp_address_number" id="tp_address_number" class="form-control"
                         value="{{ $entreprise->tp_address_number }}">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-4">
-                    <label for="">CT_TAXPAYER</label>
+                    <label for="">Assujetti à la taxe de consommation</label>
 
                     <select name="ct_taxpayer" id="" class="form-control">
 
@@ -56,7 +62,7 @@
                     </select>
                 </div>
                 <div class="col-4">
-                    <label for="">TP_FISCAL_CENTER</label>
+                    <label for="">CENTRE FISCAL</label>
 {{--                    <input type="text" name="tp_fiscal_center" id="tp_fiscal_center" class="form-control"--}}
 {{--                        value="{{ $entreprise->tp_fiscal_center }}">--}}
                     <select name="tp_fiscal_center" id="tp_fiscal_center" class="form-control">
@@ -69,14 +75,14 @@
                     </select>
                 </div>
                 <div class="col-4">
-                    <label for="">TP_LEGAL_FORM</label>
+                    <label for="">FORME JURDIQUE</label>
                     <input type="text" name="tp_legal_form" id="tp_legal_form" class="form-control"
                         value="{{ $entreprise->tp_legal_form }}">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-4">
-                    <label for="">TP_TYPE</label>
+                    <label for="">TYPE DE SOCIETE</label>
 
                     <select name="tp_type" id="" class="form-control">
                         @foreach (['',"personne physique","personne morale" ] as $key => $value)
@@ -90,29 +96,29 @@
                     </select>
                 </div>
                 <div class="col-4">
-                    <label for="">TP_TRADE_NUMBER : <b>{{ $entreprise->tp_trade_number }}</b></label>
+                    <label for="">REGISTRE DE COMERCE : <b>{{ $entreprise->tp_trade_number }}</b></label>
                     <input hidden type="text" name="tp_trade_number" id="tp_trade_number" class="form-control"
                         value="{{ $entreprise->tp_trade_number }}">
                 </div>
                 <div class="col-4">
-                    <label for="">TP_PHONE_NUMBER</label>
+                    <label for="">TELEPHONE </label>
                     <input type="text" name="tp_phone_number" id="tp_phone_number" class="form-control"
                         value="{{ $entreprise->tp_phone_number }}">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-4">
-                    <label for="">TP_ADDRESS_QUARTIER</label>
+                    <label for="">QUARTIER</label>
                     <input type="text" name="tp_address_quartier" id="tp_address_quartier" class="form-control"
                         value="{{ $entreprise->tp_address_quartier }}">
                 </div>
                 <div class="col-4">
-                    <label for="">TP_ADDRESS_RUE</label>
+                    <label for="">RUE</label>
                     <input type="text" name="tp_address_rue" id="tp_address_rue" class="form-control"
                         value="{{ $entreprise->tp_address_rue }}">
                 </div>
                 <div class="col-4">
-                    <label for="">VAT_TAXPAYER</label>
+                    <label for="">Assujetti à la TVA</label>
 
                     <select name="vat_taxpayer" id="" class="form-control">
                         @foreach ([ "NON Assujetti à la TVA","Assujetti à la TVA"] as $key => $value)
@@ -127,7 +133,7 @@
             </div>
             <div class="form-group row">
                 <div class="col-4">
-                    <label for="" class="text-">TL_TAXPAYER</label>
+                    <label for="" class="text-">Assujetti au prélèvement forfaitaire</label>
 
                     <select name="tl_taxpayer" id="" class="form-control">
                         @foreach ([ "NON Assujetti au prélèvement forfaitaire ","Assujetti au prélèvement forfaitaire "] as $key => $value)
@@ -140,12 +146,12 @@
                     </select>
                 </div>
                 <div class="col-4">
-                    <label for="">TP_ACTIVITY_SECTOR</label>
+                    <label for="">SECTEUR D'ACTIVITE</label>
                     <input type="text" name="tp_activity_sector" id="tp_activity_sector" class="form-control"
                         value="{{ $entreprise->tp_activity_sector }}">
                 </div>
                 <div class="col-4">
-                    <label for="">PAYMENT_TYPE</label>
+                    <label for="">TYPE DE PAIMENT</label>
 
                     <select name="payment_type" class="form-control">
 
@@ -157,8 +163,6 @@
                             >{{$val}}</option>
                         @endforeach
                     </select>
-
-
 
                 </div>
             </div>
