ef78994e7e4bb0f7a460b0c2c98e794e74772e81
Jean-Lionel <nijeanlionel@gmailcom>
2024-04-03 04:17:19 +0200

    generate random data

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -10,32 +10,41 @@ use Illuminate\Support\Str;
 class ProductFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+    * The name of the factory's corresponding model.
+    *
+    * @var string
+    */
     protected $model = Product::class;
 
     /**
-     * Define the model's default state.
-     *name
-code_product
-name
-quantite
-price
-date_expiration
+    * Define the model's default state.
+    *name
+    code_product
+    name
+    quantite
+    price
+    date_expiration
 
-     * @return array
-     */
+    * @return array
+    */
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->getRandomItem() .'_'. Str::random(10) ,//$this->faker->name,
             'code_product' => Str::random(10),
             'price' => $this->faker->buildingNumber,
-            'quantite' => $this->faker->randomDigitNot(20),
+            'quantite' => 0,
             'date_expiration' => $this->faker->date,
             'category_id' => Category::all()->random()->id
         ];
+    }
+
+    public function getRandomItem(){
+        $table = array("APPLE", "BANANA", "ORANGE", "GRAPE", "KIWI", "MANGO", "PAPAYA", "PINEAPPLE", "STRAWBERRY" , "STYLO" , "LAMPE" , "CISEAUX", "TABLETTE", "ORDINATEUR", "ECRAN", "CLOUS", "CIMENT", "TOLES", "PAPIER", "CLAVIER");
+        // Get a random key from the array
+        $random_key = array_rand($table);
+
+        // Get the value associated with the random key
+       return  $table[$random_key];
     }
 }
