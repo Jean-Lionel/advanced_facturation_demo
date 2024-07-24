ff7cb5b3aad2e40e74f841036a356c7d59cf38d5
Jean-Lionel <nijeanlionel@gmailcom>
2024-07-24 10:03:16 +0200

    igurire cash

--- app/Helpers/confuguration.php
+++ app/Helpers/confuguration.php
@@ -1,7 +1,7 @@
 <?php
 
 // Variable pour la syncronisation des donnees dans OBR
-define('CAN_SYNCRONISE', true);
+define('CAN_SYNCRONISE', false);
 define('TIME_OUT_SYNCRONISATION', (3 * 1000)); // Seconde de Syncronisation dans OBR
 define('CAN_SYNCRONISE_STOCK', true); // Seconde de Syncronisation dans OBR
 define('CAN_SYNCRONISE_INVOICE', true); // Seconde de Syncronisation dans OBR
@@ -12,8 +12,8 @@ define('BASE_UNITE_EMBALLAGE', 50);
 define('DAY_FOR_STOCK_DATA_SYNCRONIZE', 10);
 define('RAISON_ENTREPRISE_HEADER', 'ADVANCED FACTURATION');
 define('ARRONDIR_RESULTAT', true);
-define('USE_ABONEMENT', false);
-define('USE_LOCATION', true);
+define('USE_ABONEMENT', true);
+define('USE_LOCATION', false);
 define('USE_LOGO', true);
 define('USE_LOGO_NAME', true);
 define('LOGO_NAME', "galerie_ideal.jpg");
