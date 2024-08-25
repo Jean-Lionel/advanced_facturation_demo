47d90859881fc9ee47e2f07bd976e221a67f17cd
NiBienvenu <nicibienvenu@gmail.com>
2024-08-25 15:51:52 +0200

    partage d'interet affichage des les totaux

--- app/Http/Controllers/RapportController.php
+++ app/Http/Controllers/RapportController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use App\Models\OrderInteret;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RapportController extends Controller
@@ -15,7 +17,64 @@ class RapportController extends Controller
     }
 
     public function partage_interet(){
-        $interets = OrderInteret::latest()->paginate();
-        return view('reports.partage', compact('interets'));
+        $interets = OrderInteret::latest()->get();
+
+
+         // Initialiser les tableaux pour stocker les totaux et les noms
+         $commissionnaireTotals = [];
+         $clientTotals = [];
+         $entrepriseTotal = 0;
+         $informaticienTotal = 0;
+
+        $commissionnaires = [];
+        $clients = [];
+
+         // Parcourir tous les éléments du tableau
+         foreach ($interets as $interet) {
+            $description = json_decode($interet->description, true);
+
+            if ($description) {
+                $partage = $description['partage'] ?? [];
+                $commissionnaireId = $description['commissionaire_id'];
+                $clientId = $description['client_id'];
+
+                //  l'informaticien
+                if (isset($partage['Informaticien'])) {
+                    $informaticienTotal += $partage['Informaticien'];
+                }
+
+                // l'entreprise
+                if (isset($partage['Entreprise'])) {
+                    $entrepriseTotal += $partage['Entreprise'];
+                }
+
+                //  commissionnaire
+                if ($commissionnaireId) {
+                    if (!isset($commissionnaireTotals[$commissionnaireId])) {
+                        $commissionnaireTotals[$commissionnaireId] = 0;
+                    }
+                    $commissionnaireTotals[$commissionnaireId] += $partage['Commisionnaire'] ?? 0;
+                }
+
+                // client
+                if (!isset($clientTotals[$clientId])) {
+                    $clientTotals[$clientId] = 0;
+                }
+                $clientTotals[$clientId] += $partage['Client'] ?? 0;
+
+                if ($commissionnaireId) {
+                    $commissionnaires[] = $commissionnaireId;
+                }
+                if ($clientId) {
+                    $clients[] = $clientId;
+                }
+            }
+        }
+
+        $commissionnairesData = Client::whereIn('id', $commissionnaires)->pluck('name', 'id');
+        $clientsData = Client::whereIn('id', $clients)->pluck('name', 'id');
+
+        return view('reports.partage', compact('interets','commissionnaireTotals', 'clientTotals', 'entrepriseTotal','informaticienTotal','commissionnairesData','clientsData'));
+
     }
 }
