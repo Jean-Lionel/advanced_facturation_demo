b1420c46a4213bb230f0e2879cca3ed95ef5bd0f
NiBienvenu <nicibienvenu@gmail.com>
2025-01-02 10:15:53 +0200

    history comptes

--- resources/views/compte/historique.blade.php
+++ resources/views/compte/historique.blade.php
@@ -2,26 +2,52 @@
 
 @section('content')
 <div class="container">
-    @foreach($historiques as $historique)
-    <div class="mt-2">
-        <div class="shadow-lg row">
-            <div class="mt-4 ml-3">
-                <img src="{{ asset('img/deposit.png') }}" style="width:40px;height:40px;" alt="">
-            </div>
-            <div class="col-10">
-                <h6 class="ml-4 mt-4 " style="font-weight: bold">
-                    {{$historique->title}}
-                </h6>
-                <p style="font-size:16px;" class="ml-4 text-gray-600">{{$historique->description}} </p>
-            </div>
-            <div class="col-1 mt-3">
-                <p class="btn btn-info fw-bold">
-                    {{$historique->mode_payement}}
-                </p>
-            </div>
-        </div>
+    @php
+        $count = 1;
+    @endphp
+    <h3 class="text-center mb-4">Historique</h3>
+    <table id="historique-table" class="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    {{-- <th>Compte ID</th>
+                    <th>Client ID</th> --}}
+                    <th>Mode de Paiement</th>
+                    <th>Titre</th>
+                    <th>Montant</th>
+                    <th>Description</th>
+                    <th>Utilisateur</th>
+                    <th>Date de Création</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($historiques as $historique)
+                <tr>
+                    <td>{{ $count }}</td>
+                    {{-- <td>{{ $historique->compte_id }}</td>
+                    <td>{{ $historique->client_id }}</td> --}}
+                    <td>{{ $historique->mode_payement }}</td>
+                    <td>{{ $historique->title }}</td>
+                    <td>{{ $historique->montant }}</td>
+                    <td>{{ $historique->description }}</td>
+                    <td>{{ $historique->user->name }}</td>
+                    <td>{{ $historique->created_at->format('d/m/Y H:i') }}</td>
+                </tr>
+                @php
+                    $count++;
+                @endphp
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    @endforeach
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#historique-table').DataTable();
+        });
+    </script>
 
 
 </div>
