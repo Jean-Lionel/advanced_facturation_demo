aebf48bc55aab16afbe3301377f8d5184a5607ec
Jean-Lionel <nijeanlionel@gmailcom>
2025-03-19 08:50:21 +0200

    auth controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CacheAdvancedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +17,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('clients', [CacheAdvancedController::class, 'clients']);
+    Route::apiResource('user', App\Http\Controllers\Api\UserController::class);
+    Route::apiResource('organisation', App\Http\Controllers\Api\OrganisationController::class);
+    Route::apiResource('member', App\Http\Controllers\Api\MemberController::class);
+    Route::apiResource('transaction-type', App\Http\Controllers\Api\TransactionTypeController::class);
+    Route::apiResource('transaction', App\Http\Controllers\Api\TransactionController::class);
+    Route::apiResource('transaction-file', App\Http\Controllers\Api\TransactionFileController::class);
+    Route::apiResource('notification', App\Http\Controllers\Api\NotificationController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
 });
 
-Route::get('clients', [CacheAdvancedController::class, 'clients']);
-Route::apiResource('user', App\Http\Controllers\Api\UserController::class);
-Route::apiResource('organisation', App\Http\Controllers\Api\OrganisationController::class);
-Route::apiResource('member', App\Http\Controllers\Api\MemberController::class);
-Route::apiResource('transaction-type', App\Http\Controllers\Api\TransactionTypeController::class);
-Route::apiResource('transaction', App\Http\Controllers\Api\TransactionController::class);
-Route::apiResource('transaction-file', App\Http\Controllers\Api\TransactionFileController::class);
-Route::apiResource('notification', App\Http\Controllers\Api\NotificationController::class);
+
