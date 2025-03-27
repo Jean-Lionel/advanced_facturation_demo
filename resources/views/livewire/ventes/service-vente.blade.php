a4aa1203c7f176617aebd4dd2286b363675d9106
Jean-Lionel <nijeanlionel@gmailcom>
2025-03-27 05:11:05 +0200

    priortize

--- resources/views/livewire/ventes/service-vente.blade.php
+++ resources/views/livewire/ventes/service-vente.blade.php
@@ -56,7 +56,7 @@
                 </td>
                 <td>
                     <select class="form-control form-control-sm" wire:model="taxes.{{ $key }}">
-                        @foreach ([18,10,4,0] as $v )
+                        @foreach ([0,4,10,18] as $v )
                         <option value="{{ $v }}" @if ( isset($taxes[$key]) and $v == $taxes[$key])
                         selected
                         @endif> {{ $v }} %</option>
