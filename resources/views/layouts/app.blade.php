dc7051c30b6900a14f44fb867ba45652ad0730a3
Jean-Lionel <nijeanlionel@gmailcom>
2025-06-21 10:18:58 +0200

    remove validation on depense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -296,7 +296,7 @@
                     if(canSyncronize && !cancel_syncronize){
                         let  limitedInterval =  setInterval(async () => {
                             const result = await updateInternetStatus();
-                            console.log(result);
+                                console.log(result);
                             if(result){
                                 // window.location.reload();
                                 clearInterval(limitedInterval);
