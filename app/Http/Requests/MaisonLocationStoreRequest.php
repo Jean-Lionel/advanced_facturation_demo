4606a4d3823882c2091d6754c57966f0da623329
Jean-Lionel <nijeanlionel@gmailcom>
2025-06-23 12:34:14 +0200

    nouveau

--- app/Http/Requests/MaisonLocationStoreRequest.php
+++ app/Http/Requests/MaisonLocationStoreRequest.php
@@ -28,7 +28,7 @@ class MaisonLocationStoreRequest extends FormRequest
             'name' => ['required', 'string', 'unique:maison_locations'],
             'description' => ['string', 'nullable'],
             'montant' => ['required', 'numeric'],
-            'avance' => ['required', 'numeric'],
+           // 'avance' => ['required', 'numeric'],
             'tax' => ['required'],
 
         ];
