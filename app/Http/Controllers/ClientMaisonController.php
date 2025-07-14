bc0d6385a8441d78bd3b0f187e4ebf24d6fa2866
Jean-Lionel <nijeanlionel@gmailcom>
2025-07-14 16:14:37 +0200

    adding liste

--- app/Http/Controllers/ClientMaisonController.php
+++ app/Http/Controllers/ClientMaisonController.php
@@ -29,7 +29,7 @@ class ClientMaisonController extends Controller
                         ->whereHas('clients')
                         ->where(function($query) use($shop_letter, $customerName){
                             if($shop_letter != null && $shop_letter != "TOUS"){
-                                $query->where('name', 'like', "%{$shop_letter}%");
+                                $query->where('name', 'like', "{$shop_letter}%");
                             }
                             if($customerName != null){
                                 $query->where('name', '=', $customerName);
