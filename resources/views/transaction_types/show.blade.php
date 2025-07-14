a946666e47dcd266405703624725e919a9eac439
Jean-Lionel <nijeanlionel@gmailcom>
2025-07-14 19:31:48 +0200

    adding listes

--- resources/views/transaction_types/show.blade.php
+++ resources/views/transaction_types/show.blade.php
@@ -6,31 +6,31 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <h4>{{ $type->name }}</h4>
+                    <h4>{{ $transaction_type->name }}</h4>
                 </div>
                 <div class="card-body">
                     <div class="mb-3">
                         <strong>Description:</strong>
-                        <p>{{ $type->description }}</p>
+                        <p>{{ $transaction_type->description }}</p>
                     </div>
 
                     <div class="mb-3">
                         <strong>Créé par:</strong>
-                        <p>{{ $type->user->name }}</p>
+                        <p>{{ $transaction_type->user->name }}</p>
                     </div>
 
                     <div class="mb-3">
                         <strong>Créé le:</strong>
-                        <p>{{ $type->created_at->format('d/m/Y H:i') }}</p>
+                        <p>{{ $transaction_type->created_at->format('d/m/Y H:i') }}</p>
                     </div>
 
                     <div class="mb-3">
                         <strong>Transactions associées:</strong>
-                        <p>{{ $type->transactions->count() }} transactions</p>
+                        <p>{{ $transaction_type->transactions->count() }} transactions</p>
                     </div>
 
                     <div class="d-flex justify-content-end">
-                        <a href="{{ route('advanced.transaction_types.edit', $type) }}" class="btn btn-warning me-2">
+                        <a href="{{ route('advanced.transaction_types.edit', $transaction_type) }}" class="btn btn-warning me-2">
                             <i class="fas fa-edit"></i> Modifier
                         </a>
                         <a href="{{ route('advanced.transaction_types.index') }}" class="btn btn-secondary">
