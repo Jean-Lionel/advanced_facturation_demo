c2d8841edb891ef246a9e9bc21843d7a1c2ae2bc
Jean-Lionel <nijeanlionel@gmailcom>
2025-09-29 12:33:02 +0200

    current

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -63,7 +63,7 @@ class StockController extends Controller
                         ->whereBetween('created_at', [$dateDebut, $dateFin])
                         ->sortable()
                         ->latest()
-                        ->take(20)
+                        ->take(15)
                         ->get();
 
                 $pdf = Pdf::loadView('stocks.impression', compact('orders'));
