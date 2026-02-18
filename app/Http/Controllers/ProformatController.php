5f90edf6115e5df789380d9953c2758f31dc73e6
Jean-Lionel <nijeanlionel@gmailcom>
2026-02-18 10:54:34 +0200

    pagination sur le proforma

--- app/Http/Controllers/ProformatController.php
+++ app/Http/Controllers/ProformatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Proformat;
 use App\Http\Requests\StoreProformatRequest;
 use App\Http\Requests\UpdateProformatRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class ProformatController extends Controller
@@ -14,10 +15,24 @@ class ProformatController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $proformats = Proformat::all();
-        return view('proformats.index', compact('proformats'));
+        $search = $request->get('search', '');
+        $sort = $request->get('sort', 'desc'); // desc = plus récent, asc = plus ancien
+
+        $proformats = Proformat::when($search, function ($query) use ($search) {
+            $query->where(function ($q) use ($search) {
+                // Recherche par nom du client (stocké en JSON)
+                $q->whereRaw("JSON_EXTRACT(client, '$.name') LIKE ?", ["%{$search}%"])
+                  // Recherche par nom de service/produit (stocké en serialize)
+                  ->orWhere('products', 'LIKE', "%{$search}%");
+            });
+        })
+        ->orderBy('created_at', $sort === 'asc' ? 'asc' : 'desc')
+        ->paginate(15)
+        ->withQueryString();
+
+        return view('proformats.index', compact('proformats', 'search', 'sort'));
     }
 
     /**
@@ -89,6 +104,9 @@ class ProformatController extends Controller
      */
     public function destroy(Proformat $proformat)
     {
-        //
+        $proformat->delete();
+
+        return redirect()->route('proformats.index')
+            ->with('success', 'Proforma supprimé avec succès.');
     }
 }
--- resources/views/proformats/index.blade.php
+++ resources/views/proformats/index.blade.php
@@ -8,9 +8,43 @@
 
 	<div class="col-md-12">
 		@include('journals.header')
-
-
 	</div>
+
+    <div class="col-md-12 mb-3">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Liste des Proformas</h5>
+            </div>
+            <div class="col-md-5">
+                <form action="{{ route('proformats.index') }}" method="GET" id="searchForm">
+                    <input type="hidden" name="sort" value="{{ $sort ?? 'desc' }}">
+                    <input type="search" name="search" class="form-control form-control-sm"
+                        value="{{ $search ?? '' }}"
+                        placeholder="Rechercher par client ou service...">
+                </form>
+            </div>
+            <div class="col-md-3">
+                <form action="{{ route('proformats.index') }}" method="GET" id="sortForm">
+                    <input type="hidden" name="search" value="{{ $search ?? '' }}">
+                    <select name="sort" class="form-control form-control-sm" onchange="this.form.submit()">
+                        <option value="desc" {{ ($sort ?? 'desc') === 'desc' ? 'selected' : '' }}>Plus récent</option>
+                        <option value="asc" {{ ($sort ?? 'desc') === 'asc' ? 'selected' : '' }}>Plus ancien</option>
+                    </select>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    @if(session('success'))
+        <div class="col-md-12">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    @endif
 
     <div class="col-md-12">
         <table class="table table-sm">
@@ -25,7 +59,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach($proformats as $proformat)
+                @forelse($proformats as $proformat)
                 <tr>
                     <td>{{ $proformat->id }}</td>
                     <td>
@@ -35,20 +69,39 @@
                                     {{ $product['name'] }} X  {{ $product['quantite'] }}
                                     <b>{{ number_format($product['price'], 2) }}</b>
                                 </li>
-
                             @endforeach
                         </ul>
                     </td>
-                    <td>{{ $proformat->client->name }}</td>
+                    <td>{{ $proformat->client->name ?? '-' }}</td>
                     <td><b>{{ number_format($proformat->amount, 2) }}</b></td>
                     <td>{{ $proformat->created_at }}</td>
                     <td>
-                        <a href="{{ route('proformats.show', $proformat) }}" class="mr-2 btn btn-sm btn-success" title="imprimer"> <i class="fa fa-print" ></i></a>
+                        <div class="d-flex">
+                            <a href="{{ route('proformats.show', $proformat) }}" class="mr-2 btn btn-sm btn-success" title="Imprimer">
+                                <i class="fa fa-print"></i>
+                            </a>
+                            <form action="{{ route('proformats.destroy', $proformat) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger" title="Supprimer"
+                                    onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce proforma ?')">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
-                @endforeach
+                @empty
+                <tr>
+                    <td colspan="6" class="text-center">Aucun proforma trouvé.</td>
+                </tr>
+                @endforelse
             </tbody>
         </table>
+
+        <div class="d-flex justify-content-center">
+            {{ $proformats->links() }}
+        </div>
     </div>
 </div>
 
