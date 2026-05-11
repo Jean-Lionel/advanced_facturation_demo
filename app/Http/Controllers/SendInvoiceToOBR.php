ddb77b40e235d17b8c1411d4b86ba2cbc402b925
Jean-Lionel <nijeanlionel@gmailcom>
2026-05-11 16:13:53 +0200

    add generate documentation

--- app/Http/Controllers/SendInvoiceToOBR.php
+++ app/Http/Controllers/SendInvoiceToOBR.php
@@ -17,7 +17,8 @@ class SendInvoiceToOBR extends Controller
 
     public function __construct()
     {
-        $this->baseUrl = env('OBR_PRODUCTION', false) == true ? 'https://ebms.obr.gov.bi:8443/ebms_api/' : 'https://ebms.obr.gov.bi:9443/ebms_api/';
+        $this->baseUrl = env('OBR_PRODUCTION', false) == true ? '
+        ' : 'https://ebms.obr.gov.bi:9443/ebms_api/';
     }
 
     public function addStockMovement($data){
