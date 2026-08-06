be0932d05e177757d27062f6723c13e2d9dfee71
schammahmuhizi <schammah97@gmail.com>
2026-08-06 13:22:30 +0200

    Refactor navigation tabs to use app-tabs class for consistency

--- resources/views/ventes/_header.blade.php
+++ resources/views/ventes/_header.blade.php
@@ -1,29 +1,24 @@
 <link rel="stylesheet" href="{{ asset('css/ventes.css') }}">
 
-<nav class="vente-tabs noprint" aria-label="Navigation vente">
+<nav class="app-tabs noprint" aria-label="Navigation vente">
     <a href="{{ route('ventes.index') }}"
-       class="vente-tab {{ request()->routeIs(['ventes.index', 'home']) ? 'is-active' : '' }}">
-        <i class="fas fa-box-open"></i>
-        <span>Vente produits</span>
+       class="{{ request()->routeIs(['ventes.index', 'home']) ? 'is-active' : '' }}">
+        <span class="fas fa-box-open"></span> Vente produits
     </a>
     <a href="{{ route('ventes.create') }}"
-       class="vente-tab {{ request()->routeIs('ventes.create') ? 'is-active' : '' }}">
-        <i class="fas fa-file-invoice"></i>
-        <span>Services</span>
+       class="{{ request()->routeIs('ventes.create') ? 'is-active' : '' }}">
+        <span class="fas fa-file-invoice"></span> Services
     </a>
     <a href="{{ route('facture.avoir') }}"
-       class="vente-tab {{ request()->routeIs('facture.avoir') ? 'is-active' : '' }}">
-        <i class="fas fa-file-invoice-dollar"></i>
-        <span>Facture d'avoir</span>
+       class="{{ request()->routeIs('facture.avoir') ? 'is-active' : '' }}">
+        <span class="fas fa-file-invoice-dollar"></span> Facture d'avoir
     </a>
     <a href="{{ route('facture.remboursement_caution') }}"
-       class="vente-tab {{ request()->routeIs('facture.remboursement_caution') ? 'is-active' : '' }}">
-        <i class="fas fa-hand-holding-usd"></i>
-        <span>Caution</span>
+       class="{{ request()->routeIs('facture.remboursement_caution') ? 'is-active' : '' }}">
+        <span class="fas fa-hand-holding-usd"></span> Caution
     </a>
     <a href="{{ route('panier.index') }}"
-       class="vente-tab {{ request()->routeIs('panier.*') ? 'is-active' : '' }}">
-        <i class="fas fa-shopping-cart"></i>
-        <span>Panier</span>
+       class="{{ request()->routeIs('panier.*') ? 'is-active' : '' }}">
+        <span class="fas fa-shopping-cart"></span> Panier
     </a>
 </nav>
